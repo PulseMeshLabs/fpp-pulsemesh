f549d783e2d50240f425d08057812a8849364b65
rdkbob <rdkbob@gmail.com>
2024-10-07 15:14:36 -0400

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -7,12 +7,14 @@ cd $BASEDIR
 cd ..
 make "SRCDIR=${SRCDIR}"
 
-wget -O - https://repo.pulsemesh.io/pulsemsh-repo-key.gpg | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf] https://repo.pulsemesh.io stable main" > /etc/apt/sources.list.d/pulsemsh.list'
-sudo apt update -o Dir::Etc::sourcelist="/etc/apt/sources.list.d/pulsemsh.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+wget -O /usr/share/keyrings/pulsemsh-repo-key.gpg https://repo.pulsemesh.io/pulsemsh-repo-key.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/pulsemsh-repo-key.gpg] https://repo.pulsemesh.io stable main" | sudo tee /etc/apt/sources.list.d/pulsemsh.list
+
+sudo apt update
 sudo apt install -y pulsemesh-connector
+
 sudo systemctl enable pulsemesh-connector.service
 sudo systemctl start pulsemesh-connector.service
 
-. ${FPPDIR}/scripts/common
+. "${FPPDIR}/scripts/common"
 setSetting restartFlag 1
