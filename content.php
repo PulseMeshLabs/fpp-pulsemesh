54843f93c875b308b44158cb6955829003aba4e4
Bob Kazan <rdkbob@gmail.com>
2025-06-17 06:43:54 -0400

    UI Cleanup

--- content.php
+++ content.php
@@ -4,7 +4,7 @@
         $host = $_SERVER['SERVER_NAME'] ?? 'localhost';
         $url = $protocol . $host . ":8089";
         echo '<div>';
-        echo '<iframe src="' . htmlspecialchars($url) . '" width="100%" height="1000" frameborder="0" style="border: 1px solid #ccc; border-radius: 20px;"></iframe>';
+        echo '<iframe src="' . htmlspecialchars($url) . '" width="100%" height="1000" frameborder="0" style="border: 1px solid #ccc; border-radius: 8px;"></iframe>';
         echo '</div>';
 ?>
 </div>
